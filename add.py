cf28105e787627b033f9f89bb76b1095f6f96c50
sandeepdevopscgmailcom <sandeepdevopsc@gmail.com>
2024-05-15 18:57:14 +0530

    file added

---
+++ add.py
@@ -0,0 +1,7 @@
+print('Addition of 2 numbers')
+num1 = int(input ('enter the First number'))
+num2 = int(input ('enter the Second number'))
+
+total = num1 + num2
+
+print('sum of two numbers is {total}')
